9969a8df79c1382d59f48c7cb4ccff684bd6a206
Christophe Ameller <christopheameller@MacBook-Pro-de-Christophe-2.local>
2020-11-18 19:09:21 +0100

    first save for marvel backend

---
+++ index.js
@@ -0,0 +1,44 @@
+require("dotenv").config();
+const express = require("express");
+const formidable = require("express-formidable");
+const axios = require("axios");
+const MD5 = require("crypto-js/md5");
+
+const app = express();
+
+//création de la timestamp
+const date = new Date();
+const timeStamp = Math.floor(date.getTime() / 1000);
+const hashValue = MD5(
+  timeStamp + process.env.API_KEY + process.env.API_PUBLIC_KEY
+);
+
+app.get("/characters", async (req, res) => {
+  try {
+    const response = await axios.get(
+      `http://gateway.marvel.com/v1/public/characters?ts=${timeStamp}&apikey=${process.env.API_PUBLIC_KEY}&hash=${hashValue}`
+    );
+    res.json(response.data.data.results);
+  } catch (error) {
+    console.log(error.response);
+  }
+});
+
+app.get("/comics", async (req, res) => {
+  try {
+    const response = await axios.get(
+      `http://gateway.marvel.com/v1/public/comics?ts=${timeStamp}&apikey=${process.env.API_PUBLIC_KEY}&hash=${hashValue}`
+    );
+    res.json(response.data.data.results);
+  } catch (error) {
+    console.log(error.response);
+  }
+});
+
+app.all("*", (req, res) => {
+  res.json("Cette route n'existe pas");
+});
+
+app.listen(3000, () => {
+  console.log("Server started");
+});
